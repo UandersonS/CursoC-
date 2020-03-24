1395b53b82a576bcdbf10e1f52df8292583bb128
UandersonS <58696209+UandersonS@users.noreply.github.com>
2020-03-24 18:19:05 -0300

    Add files via upload

---
+++ Seção 4/ex006/ex006/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex006
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+        public static double ValorConvertido(double quantia, double cotacao)
+        {
+            double total = quantia * cotacao;
+            return total + total * Iof / 100.0;
+        }
+    }
+}
---
+++ Seção 4/ex006/ex006/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a cotação do Dólar?");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantos dólares voce vai comprar?");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = ConversorDeMoeda.ValorConvertido(quantia, cotacao);
+
+            Console.WriteLine("Valor pago em reais = " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
