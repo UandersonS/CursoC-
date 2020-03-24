5d46f9e92c771b8d6b271291eefea2425b50b468
UandersonS <58696209+UandersonS@users.noreply.github.com>
2020-03-24 18:22:10 -0300

    Add files via upload

---
+++ MembrosEstaticos 2/MembrosEstaticos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Entre com o valor do raio");
+
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double cir = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + cir.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2"), CultureInfo.InvariantCulture);
+            Console.WriteLine("PI: " + Calculadora.Pi);
+        }
+    }
+}
