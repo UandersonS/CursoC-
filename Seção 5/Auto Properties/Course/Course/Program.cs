6cadda38f7819b046fc368b5efb52cdfd640e7d6
UandersonS <58696209+UandersonS@users.noreply.github.com>
2020-04-07 18:54:19 -0300

    Add files via upload

---
+++ Seção 5/Auto Properties/Course/Course/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = ("TV");
+
+            Console.WriteLine(p.Nome);
+        }
+    }
+}
